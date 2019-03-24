b094fb5e117887230d0f55ec9a5b98d178eead82
Daniel Hernandez <dhdan97@gmail.com>
2019-03-24 00:21:47 -0400

    finished Q1 and Q4

--- hw4q1.cpp
+++ hw4q1.cpp
@@ -1,15 +1,64 @@
+#ifndef __HW4Q1_CPP__
+#define __HW4Q1_CPP__
+
 #include "hw4q1.h"
 
 template <class Item>
-Stack<item>::Stack(){
-	data = new Item[DEF_CAP];
+Stack<Item>::Stack(){
+	data=new Item[DEF_CAP];
 	used=0;
 }
 
 template <class Item>
-Stack<item>::Stack(const Stack& source){
+Stack<Item>::Stack(const Stack& source){
 	data = new Item[DEF_CAP];
 	used=source.used;
-	for(int i=0;i<_size;i++)
+	for(int i=0;i<used;i++)
         data[i] = source.data[i];
 }
+
+template<class Item>
+Stack<Item>::~Stack(){
+	delete [] data;
+}
+
+template<class Item>
+void Stack<Item>::push(const Item& entry){
+	//assert(used<DEF_CAP);
+	data[used]=entry;
+	used++;
+}
+
+template<class Item>
+void Stack<Item>::pop(){
+	if(!empty())
+		used--;
+}
+
+template<class Item>
+void Stack<Item>::operator=(const Stack<Item>& source){
+	if(this!=&source){
+		data = new Item[DEF_CAP];
+		used=source.used;
+		for(size_t i=0;i<used;i++)
+			data[i]=source.data[i];
+	}
+}
+
+template<class Item>
+size_t Stack<Item>::size() const{
+	return used;
+}
+
+template<class Item>
+bool Stack<Item>::empty() const{
+	return(used==0);
+}
+
+template<class Item>
+Item Stack<Item>::top() const{
+	if(!empty())
+		return data[used-1];
+}
+
+#endif
