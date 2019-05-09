5a508bcb9f713e299626c0efd2f9aae23b6ad890
Daniel Hernandez <dhdan97@gmail.com>
2019-05-09 15:22:31 -0400

    line class done

--- exam1/main.cpp
+++ exam1/main.cpp
@@ -1,3 +1,6 @@
+#ifndef LINE_MAIN
+#define LINE_MAIN
+
 #include <iostream>
 #include "line.h"
 using namespace std;
@@ -5,5 +8,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	line<int> l1;
+	cout<<l1.getPoint1()<<"\n";
 	return 0;
 }
+#endif
